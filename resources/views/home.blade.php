b308f6425c41c6f0a9995014d7db92f6cac2ad07
Felipe <felipe.stec@gmail.com>
2023-09-04 20:28:09 -0300

    Primeiro Commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Sistema Mentor</h1>
+    <div><h2>Faça seu login: </h2>
+    <form action="#" method="POST">
+         <input type="text">
+         <input type="text">
+    </form>
+</div>
+
+<div>
+    <h2> Faça seu cadastro: </h2>
+    <form action="#" method="POST">
+        <label for="cadastro-login">Login: </label>
+        <input type="text">
+
+        <label for="cadastro-email">Email: </label>
+        <input type="text">
+
+        <label for="cadastro-senha">Senha: </label>
+        <input type="password">
+
+        <label for="cadastro-senha-confirmacao">Confirme sua senha: </label>
+        <input type="password">
+    </form>
+</div>
+</body>
+</html>
